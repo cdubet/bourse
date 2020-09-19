6328d52761ce832b6b936ba250de4820b2deed3b
christian dubettier <cdubet@gmx.net>
2020-09-19 16:29:23 +0200

    with picocli

--- test/src/main/java/test/testMain.java
+++ test/src/main/java/test/testMain.java
@@ -1,17 +1,36 @@
 package test;
 
-public class testMain
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+public class testMain  implements Runnable 
 {
+	@Option(names = "--user")
+	String user;
+
+	@Option(names = "--password", arity = "0..1", interactive = true)
+	String password;
+	//private static Connection conn;             //our connection to the db - persist for life of program
 
 	public testMain()
 	{
 		// TODO Auto-generated constructor stub
 	}
+    public void run() {
+        if (user.length() > 0) {
+            System.out.println(user.length() + " user..."+user);
+        }
+        if (password.length() > 0) {
+            System.out.println(password.length() + " password..."+password);
+        }
+    }
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+   	 new CommandLine(new testMain()).execute(args);
 		System.out.print("coucu");
+//		DSLContext create = DSL.using(conn, SQLDialect.HSQLDB);
+
 	}
 
 
