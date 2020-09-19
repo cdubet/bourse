c1001125e3b99b711637ddf29707366bdfd5fbff
christian dubettier <cdubet@gmx.net>
2020-09-19 13:02:40 +0200

    initial test

---
+++ test/src/main/java/test/testMain.java
@@ -0,0 +1,19 @@
+package test;
+
+public class testMain
+{
+
+	public testMain()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.print("coucu");
+	}
+
+
+
+}
