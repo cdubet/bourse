304932bf37025c46ec812c846f7bf4a9b3a2ef56
christian dubettier <cdubet@gmx.net>
2020-09-30 22:17:40 +0200

    remove typo in dir name

--- bourse/src/test/java/portefeuilleTest/AllTests.java
+++ bourse/src/test/java/portefeuilleTest/AllTests.java
@@ -1,4 +1,4 @@
-package protefeuilleTest;
+package portefeuilleTest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
--- bourse/src/test/java/portefeuilleTest/TestCvs.java
+++ bourse/src/test/java/portefeuilleTest/TestCvs.java
@@ -1,4 +1,4 @@
-package protefeuilleTest;
+package portefeuilleTest;
 
 import static org.junit.Assert.assertEquals;
 
